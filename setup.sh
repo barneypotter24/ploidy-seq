d32b3bb13f84f6c2f1a4cf923dc341b4de2ae7ef
Barney Potter <barney.potter.24@gmail.com>
2017-08-04 15:13:02 -0700

    Add trimmomatic rule to pipeline.

--- setup.sh
+++ setup.sh
@@ -25,43 +25,43 @@ mkdir htseq
 # Populate data directories from CyVerse cloud
 
 # Arabidopsis reference genome
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR10.gene_exons.gtf
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR10_spliceSites.txt
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.1.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.2.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.3.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.4.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.5.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.6.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.7.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.8.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.fa.gz
-# ERCC reference 'genome'
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.1.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.2.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.3.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.4.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.5.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.6.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.7.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.8.ht2
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.fa
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.gtf
-# FastQ transcriptome files
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WL1_CAGATC_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WL2_CAGATC_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WL3_ATCACG_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WL4_CAGATC_R1.fastq.gz
-
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WL1_CGATGT_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WL2_TTAGGC_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WL2_TTAGGC_R1.fastq.gz
-
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48974_CA73UANXX_2xWa5_CCGTCC_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48975_CA73UANXX_2xWa6_GTCCGC_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48976_CA73UANXX_2xWa7_GTGAAA_R1.fastq.gz
-
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48977_CA73UANXX_4xWa4_GTGGCC_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48978_CA73UANXX_4xWa5_GTTTCG_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48979_CA73UANXX_4xWa6_CGTACG_R1.fastq.gz
-cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48980_CA73UANXX_4xWa7_GAGTGG_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR10.gene_exons.gtf
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR10_spliceSites.txt
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.1.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.2.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.3.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.4.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.5.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.6.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.7.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.8.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/Athaliana_167_TAIR9.fa.gz
+# # ERCC reference 'genome'
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.1.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.2.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.3.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.4.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.5.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.6.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.7.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.8.ht2
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.fa
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Reference_Seqs/ERCC92.gtf
+# # FastQ transcriptome files
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WL1_CAGATC_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WL2_CAGATC_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WL3_ATCACG_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WL4_CAGATC_R1.fastq.gz
+#
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WL1_CGATGT_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WL2_TTAGGC_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WL2_TTAGGC_R1.fastq.gz
+#
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48974_CA73UANXX_2xWa5_CCGTCC_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48975_CA73UANXX_2xWa6_GTCCGC_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48976_CA73UANXX_2xWa7_GTGAAA_R1.fastq.gz
+#
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48977_CA73UANXX_4xWa4_GTGGCC_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48978_CA73UANXX_4xWa5_GTTTCG_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48979_CA73UANXX_4xWa6_CGTACG_R1.fastq.gz
+# cd reference && iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/8204_1401_48980_CA73UANXX_4xWa7_GAGTGG_R1.fastq.gz
