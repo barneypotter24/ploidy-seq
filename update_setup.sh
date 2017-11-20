b7acd67fb1dde18744a0f51be4ba24f34db5aa02
Barney Potter <barney.potter.24@gmail.com>
2017-11-20 10:13:30 -0800

    Update for Cl and Ws accessions.

---
+++ update_setup.sh
@@ -0,0 +1,26 @@
+# 2Cl
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2CL2_CGATCT_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2CL3_TGACCA_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2CL4_TGACCA_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2CL5_CTTGTA_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2CL6_AGTCAA_R1.fastq.gz
+
+# 2Ws
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WsL3_TGACCA_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WsL4_ACAGTG_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WsL5_GCCAAT_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/2WsL6_CAGATC_R1.fastq.gz
+
+# 4Cl
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4CL1_TGACCA_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4CL2_TGACCA_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4CL3_CAGATC_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4CL4_CAGATC_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4CL5_AGTTCC_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4CL6_ATGTCA_R1.fastq.gz
+
+# 4Ws
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WsL1_ACTTGA_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WsL4_GATCAG_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WsL5_TAGCTT_R1.fastq.gz
+iget -K /iplant/home/jcoate/Arabidopsis/2017/Fastq/4WsL6_GGCTAC_R1.fastq.gz
